78b2e0d25c43a8165c86b338cf1f53c949c36ffc
adorno <pch4623@gmail.com>
2020-09-30 15:42:47 +0900

    MemberRepository 및 Test 작성

--- src/main/java/jpabook/jpashop/MemberRepository.java
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -1,11 +1,13 @@
 package jpabook.jpashop;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @Repository
+@Transactional
 public class MemberRepository {
 
     @PersistenceContext
