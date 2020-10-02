8025f1d2df5e989c3da968bcc6e9ff236f5831c1
adorno <pch4623@gmail.com>
2020-10-02 22:04:57 +0900

    MemberService 작성

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -1,2 +1,45 @@
-package jpabook.jpashop.service;public class MemberService {
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.Repository.MemberRepository;
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor  // final이 붙은 필드에 대해 빈 생성자 주입
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // 회원 가입
+    @Transactional  // insert의 경우 readOnly = false
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 중복 회원 검증
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // 회원 단건 조회
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
 }
