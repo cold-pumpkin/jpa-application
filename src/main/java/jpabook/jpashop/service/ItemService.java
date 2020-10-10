5c9dc5f08cb5228e4b72fc2a8630b3d0c52b25da
adorno <pch4623@gmail.com>
2020-10-10 17:47:36 +0900

    아이템 정보 수정 Transaction 코드 개선
    엔티티를 파라미터로 받는 방식에서 수정에 필요한 필드값들만 받는 방식으로
    개선. 변경감지(Dirty checking) 방식으로 준영속 엔티티 수정.
    추후 유지보수를 위해 setter들을 따로따로 호출하기보다 setter들을 한 곳에
    모아놓는 메서드를 따로 만들어 놓는 것을 추천.

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.domain.item.Item;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,17 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        // 준영속 엔티티 수정 : 변경감지
+        Item findItem = itemRepository.findOne(itemId);  // 영속성 객체 찾아오기
+        // * 유지보수를 위해 setter 남발하는 것 보다 change 메서드를 만들어 놓는 것을 추천
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+        // 값 셋팅만 해도 @Transactional에 의해 commit -> JPA flush(영속성 중 변경된 값 찾아서 update)
     }
 
     public List<Item> findItems() {
